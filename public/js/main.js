757ca1d49b0924076ab0e40e51c430bf660268d7
user.email <job.klenchys.i@gmail.com>
2020-04-23 18:29:28 +0200

    Animation Fixed

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,21 @@
-$(document).ready(function () {
+window.addEventListener("DOMContentLoaded", function () {
+    let burger = document.querySelector('.burger'),
+        menu = document.querySelector('.header__nav');
 
+    listLi = document.querySelectorAll('.li-anim');
+    burger.addEventListener('click', function () {
+        burger.classList.toggle("burger-active");
+        menu.classList.toggle("open-menu");
+
+    });
+    // scroll-fixed
+    let boxContainer = document.getElementById('facts');
+    function fixedContainer() {
+        if(boxContainer.getBoundingClientRect().top <= 90){
+           boxContainer.classList.add('active-fixed');
+        }else {
+            boxContainer.classList.remove('active-fixed');
+        }
+    }
+    setInterval(fixedContainer, 100);
 });
